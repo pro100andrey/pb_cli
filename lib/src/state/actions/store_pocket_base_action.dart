9fb5aeb0594dbd3ab19cc987d8c6717a221ec0f2
Andrii Ivanov <thsis.andrey@gmail.com>
2025-11-17 19:19:56 +0200

    wip

--- lib/src/state/actions/store_pocket_base_action.dart
+++ lib/src/state/actions/store_pocket_base_action.dart
@@ -5,17 +5,11 @@ import 'action.dart';
 final class StorePocketBaseAction extends AppAction {
   @override
   AppState? reduce() {
-
-    
-
-
     final pocketBase = PocketBase(select.host!);
 
     // Save PocketBase instance to the store
     store.setProp(pocketBase);
     logger.detail('PocketBase instance stored.');
-
-
 
     return null;
   }
