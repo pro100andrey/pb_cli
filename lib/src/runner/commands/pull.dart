7242ef6e8ca2e750d6779f52e3de972edbbbc630
Andrii Ivanov <this.andrey@gmail.com>
2025-12-08 14:39:06 +0200

    refactor: Clean up imports in PullCommand and update local schema exports

--- lib/src/runner/commands/pull.dart
+++ lib/src/runner/commands/pull.dart
@@ -5,10 +5,8 @@ import 'package:mason_logger/mason_logger.dart';
 import '../../redux/common/app_action.dart';
 import '../../redux/config/actions/load_config_action.dart';
 import '../../redux/env/actions/load_env_action.dart';
-import '../../redux/local_schema/actions/compare_schema_with_remote_action.dart';
-import '../../redux/local_schema/actions/save_local_schema_action.dart';
 import '../../redux/local_schema/local_schema.dart';
-import '../../redux/remote_schema/actions/fetch_remote_schema_action.dart';
+import '../../redux/remote_schema/remote_schema.dart';
 import '../../redux/session/session.dart';
 import '../../redux/work_dir/work_dir.dart';
 import '../../utils/strings.dart';
@@ -67,7 +65,7 @@ class PullCommand extends Command with WithStore {
     dispatchSync(CompareSchemaWithRemoteAction());
     dispatchSync(SaveLocalSchemaAction());
 
-    final batchSize =
+    final _ =
         int.tryParse(argResults![S.batchSizeOptionName]) ??
         int.parse(S.pullBatchSizeOptionDefault);
 
